9b3c57b6438d645efcf930296b8342474c6cbda3
krbtgt <144264613+goldenkirbi@users.noreply.github.com>
2024-11-10 19:11:51 +0000

    feat: map tracking

--- AgonesPlugin.cs
+++ AgonesPlugin.cs
@@ -5,13 +5,14 @@ using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CSSTimer = CounterStrikeSharp.API.Modules.Timers.Timer;
 using CSSTimerFlags = CounterStrikeSharp.API.Modules.Timers.TimerFlags;
+using Microsoft.Extensions.Logging;
 
 namespace AgonesPlugin;
 
 public class AgonesPlugin : BasePlugin
 {
     public override string ModuleName => "AgonesPlugin";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.3";
     public override string ModuleAuthor => "krbtgt";
     public override string ModuleDescription => "Provides integration with Agones";
     private AgonesSDK agones;
@@ -25,9 +26,13 @@ public class AgonesPlugin : BasePlugin
 
     public override void Load(bool hotReload)
     {
+
+        Logger.LogInformation("[AgonesPlugin] Load: starting SDK Health Check task");
         healthTimer ??= AddTimer(1, () => {
             Task.Run(async () => await agones.HealthAsync());
         }, CSSTimerFlags.REPEAT);
+
+        Logger.LogInformation("[AgonesPlugin] Load: Setting server to Ready state");
         Task.Run(async () => await agones.ReadyAsync());
     }
 
@@ -38,17 +43,26 @@ public class AgonesPlugin : BasePlugin
     }
 
     [GameEventHandler]
-    public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
+    public HookResult OnMapLoaded(EventGameNewmap @event, GameEventInfo info)
     {
+        Task.Run(async() => await agones.SetLabelAsync("cs2-map", @event.Mapname));
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerConnect(EventPlayerConnectFull @event, GameEventInfo info)
+    {
+        Logger.LogInformation("[AgonesPlugin] OnPlayerConnect");
         CCSPlayerController? player = @event.Userid;
-        if (player == null) {
-            // TODO: LOG THIS! NEED TO FIGURE OUT IF / WHY THIS HAPPENS
+        if (player == null || player.IsHLTV || player.IsBot || !player.IsValid) {
             return HookResult.Continue;
         }
         shutdownTimer?.Kill();
         shutdownTimer = null;
-        //Task.Run(async () => await agones.Beta().IncrementCounterAsync("players", 1));
-        Task.Run(async () => await agones.Beta().AppendListValueAsync("players", player.AuthorizedSteamID?.SteamId64.ToString()));
+        Logger.LogInformation($"[AgonesPlugin] OnPlayerConnect: updating players list: {player.SteamID.ToString()}");
+        Task.Run(async () => {
+            await agones.Beta().AppendListValueAsync("players", player.SteamID.ToString());
+        });
 
         return HookResult.Continue;
     }
@@ -56,17 +70,19 @@ public class AgonesPlugin : BasePlugin
     [GameEventHandler]
     public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
     {
+        Logger.LogInformation("[AgonesPlugin] OnPlayerDisconnect");
         CCSPlayerController? player = @event.Userid;
         if (player == null) {
-            // TODO: LOG THIS! NEED TO FIGURE OUT IF / WHY THIS HAPPENS
             return HookResult.Continue;
         }
         if (player.IsBot || player.IsHLTV || !player.IsValid) {
             return HookResult.Continue;
         }
         //Task.Run(async () => await agones.Beta().DecrementCounterAsync("players", 1));
-        Task.Run(async () => await agones.Beta().DeleteListValueAsync("players", player.AuthorizedSteamID?.SteamId64.ToString()));
-        if (PlayersConnected() == 0 && shutdownTimer == null) {
+        Logger.LogInformation($"[AgonesPlugin] OnPlayerDisconnect: updating players list: {player.SteamID.ToString()}");
+        Task.Run(async () => await agones.Beta().DeleteListValueAsync("players", player.SteamID.ToString()));
+        if (PlayersConnectedExcept(player) == 0 && shutdownTimer == null) {
+            Logger.LogInformation("[AgonesPlugin] OnPlayerDisconnect: no players remain - starting shutdown timer");
             shutdownTimer = AddTimer(60, () => {
                 Task.Run(async () => await agones.ShutDownAsync());
             });
@@ -78,5 +94,10 @@ public class AgonesPlugin : BasePlugin
     private static int PlayersConnected()
     {
         return Utilities.GetPlayers().Where(player => player.IsValid && !player.IsHLTV && !player.IsBot).Count();
+    }
+
+    private static int PlayersConnectedExcept(CCSPlayerController exception)
+    {
+        return Utilities.GetPlayers().Where(player => player.IsValid && !player.IsHLTV && !player.IsBot && player.SteamID != exception.SteamID).Count();
     }
 }
